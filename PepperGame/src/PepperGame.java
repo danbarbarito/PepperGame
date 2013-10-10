fd0ff7316e413d884f7888b5974a9c5bfdd734cf
danielbarbarito <danielbarbarito@mail.adelphi.edu>
2013-10-10 10:29:03 -0400

    Basic Outline of Game

--- PepperGame/src/PepperGame.java
+++ PepperGame/src/PepperGame.java
@@ -1,12 +1,41 @@
-
-public class PepperGame {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("neckbeards is gay");
-	}
-
+import org.newdawn.slick.*;
+public class PepperGame extends BasicGame {
+ 
+    static int width = 640;
+    static int height = 480;
+   
+    static boolean fullscreen = false;
+    static boolean showFPS = true;
+    static String title = "Slick2D Basic Game Template";
+    static int fpslimit = 40;
+   
+    public PepperGame(String title) {
+        super(title);
+    }
+ 
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+       
+    }
+ 
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException {
+       
+    }
+ 
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+       
+    }
+   
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new PepperGame(title));
+        app.setDisplayMode(width, height, fullscreen);
+        app.setSmoothDeltas(true);
+        app.setTargetFrameRate(fpslimit);
+        app.setShowFPS(showFPS);
+        app.start();
+    }
+   
 }
+

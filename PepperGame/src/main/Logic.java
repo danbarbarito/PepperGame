4a98bc36bf6f90a46e9c68abff0c87896ff8b318
danielbarbarito <danielbarbarito@SWL100044446.adelphi.internal>
2013-10-24 12:22:26 -0400

    Changed comment

--- PepperGame/src/main/Logic.java
+++ PepperGame/src/main/Logic.java
@@ -9,7 +9,7 @@ public class Logic {
 	//CHANGE HEALTH TO GPA
 	//F's FALL FROM SKY
 	//EACH F CAUSES THE LOSS OF .1 GPA
-	//Some A's will appear in the middle area and cause your GPA to go up
+	//Some A's will appear in the middle area and cause your GPA to go up!
 	public double gpa;
 	public DecimalFormat df;
 	public String gpaString;
